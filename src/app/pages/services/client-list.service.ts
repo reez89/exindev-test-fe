993cd8e429e6770ffc95574f1b57e30e28f40fd1
nrizzi89 <n.rizzi@be-tse.it>
2024-10-24 14:54:32 +0200

    feat: added client card page and client card component and service

---
+++ src/app/pages/services/client-list.service.ts
@@ -0,0 +1,6 @@
+export class ClientListService {
+ // implementare le chiamate al be //
+
+  // getList https://mrkt-dsk.dev.exin.digital/api/v2/desk/partners/fideuram
+  // getItem https://mrkt-dsk.dev.exin.digital/api/v2/desk/partners/fideuram/1
+}
---
+++ src/app/shared/client-card/client-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-card.component.html',
+  styleUrl: './client-card.component.scss'
+})
+export class ClientCardComponent {
+
+}
